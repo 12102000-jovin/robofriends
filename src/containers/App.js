5db016cafbfbcb374ea8774c326c6b421688f577
12102000-jovin <jovinhampton@gmail.com>
2024-01-18 10:03:41 +1100

    newest

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,6 @@ import CardList from "../components/CardList";
 import { robots } from "../robots";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
-import ErrorBoundary from "../components/ErrorBoundary";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
